6a167abcf1178eef6e6b5d8eb2db598f8d2d23f7
사ìësinsj5858 <goddnjswns58@gmail.com>
2024-02-16 17:55:14 +0900

    클래스

--- src/BookTest.java
+++ src/BookTest.java
@@ -1,7 +1,12 @@
 public class BookTest {
     public static void main(String[] args) {
         Book book = new Book();
-        book.isbn="a";
-
+        book.title = "자바";
+        book.price = 30000;
+        book.company = "패스트캠퍼스";
+        book.author = "신원준";
+        book.page = 700;
+        book.isbn = "12345678";
+        System.out.println(book.title+"\t"+book.price+"\t"+book.company+"\t"+book.author+"\t"+book.page+"\t"+book.isbn);
     }
 }
